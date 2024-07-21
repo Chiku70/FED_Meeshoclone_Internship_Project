752d56fd2d21acd20d007b87b8e039bb8983951d
Chiku70 <sranjanpanda7@gmail.com>
2024-07-21 23:15:33 +0530

    first commit

---
+++ items.js
@@ -0,0 +1,435 @@
+const Products = {
+    items: [
+        {
+            name: "Non-Stick Cookware Set",
+            imageSrc: "images/non-stick-cookware.jpg",
+            price: 149.99
+        },
+        {
+            name: "Kitchen Knife Set",
+            imageSrc: "images/kitchen-knife.jpg",
+            price: 39.99
+        },
+        {
+            name: "Coffee Maker Machine",
+            imageSrc: "images/coffee-maker.jpg",
+            price: 89.99
+        },
+        {
+            name: "Stainless Steel Mixing Bowls",
+            imageSrc: "images/mixing-bowls.jpg",
+            price: 29.99
+        },
+        {
+            name: "Bedding Set",
+            imageSrc: "images/bedding-set.jpg",
+            price: 99.99
+        },
+        {
+            name: "Vacuum Cleaner",
+            imageSrc: "images/vacuum-cleaner.jpg",
+            price: 199.99
+        },
+        {
+            name: "LED Desk Lamp",
+            imageSrc: "images/led-lamp.jpg",
+            price: 49.99
+        },
+        {
+            name: "Dining Table Set",
+            imageSrc: "images/dining-table.jpg",
+            price: 399.99
+        },
+        {
+            name: "Water Purifier",
+            imageSrc: "images/water-purifier.jpg",
+            price: 299.99
+        },
+        {
+            name: "Toaster Oven",
+            imageSrc: "images/toaster-oven.jpg",
+            price: 79.99
+        },
+        {
+            name: "Cooking Utensil Set",
+            imageSrc: "images/cooking-utensil.jpg",
+            price: 24.99
+        },
+        {
+            name: "Air Fryer",
+            imageSrc: "images/air-fryer.jpg",
+            price: 129.99
+        },
+        {
+            name: "Non-Stick Cookware Set",
+            imageSrc: "images/non-stick-cookware.jpg",
+            price: 149.99
+        },
+        {
+            name: "Kitchen Knife Set",
+            imageSrc: "images/kitchen-knife.jpg",
+            price: 39.99
+        },
+        {
+            name: "Coffee Maker Machine",
+            imageSrc: "images/coffee-maker.jpg",
+            price: 89.99
+        },
+        {
+            name: "Stainless Steel Mixing Bowls",
+            imageSrc: "images/mixing-bowls.jpg",
+            price: 29.99
+        },
+        {
+            name: "Bedding Set",
+            imageSrc: "images/bedding-set.jpg",
+            price: 99.99
+        },
+        {
+            name: "Vacuum Cleaner",
+            imageSrc: "images/vacuum-cleaner.jpg",
+            price: 199.99
+        },
+        {
+            name: "LED Desk Lamp",
+            imageSrc: "images/led-lamp.jpg",
+            price: 49.99
+        },
+        {
+            name: "Dining Table Set",
+            imageSrc: "images/dining-table.jpg",
+            price: 399.99
+        },
+        {
+            name: "Water Purifier",
+            imageSrc: "images/water-purifier.jpg",
+            price: 299.99
+        },
+        {
+            name: "Toaster Oven",
+            imageSrc: "images/toaster-oven.jpg",
+            price: 79.99
+        },
+        {
+            name: "Cooking Utensil Set",
+            imageSrc: "images/cooking-utensil.jpg",
+            price: 24.99
+        },
+        {
+            name: "Air Fryer",
+            imageSrc: "images/air-fryer.jpg",
+            price: 129.99
+        },
+        {
+            name: "Organic Face Cream",
+            imageSrc: "images/face-cream.jpg",
+            price: 29.99
+        },
+        {
+            name: "Herbal Shampoo",
+            imageSrc: "images/shampoo.jpg",
+            price: 15.99
+        },
+        {
+            name: "Aloe Vera Gel",
+            imageSrc: "images/aloe-vera-gel.jpg",
+            price: 12.99
+        },
+        {
+            name: "Vitamin C Serum",
+            imageSrc: "images/vitamin-c-serum.jpg",
+            price: 24.99
+        },
+        {
+            name: "Natural Lip Balm",
+            imageSrc: "images/lip-balm.jpg",
+            price: 5.99
+        },
+        {
+            name: "Classic Black Suit",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Casual Blue Shirt",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Formal Grey Trousers",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "White Tuxedo",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Classic Black Suit",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Casual Blue Shirt",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Formal Grey Trousers",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "White Tuxedo",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Classic Black Suit",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Casual Blue Shirt",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Formal Grey Trousers",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "White Tuxedo",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Summer Linen Blazer",
+            imageSrc: "images/linen-blazer.jpg",
+            price: 129.99
+        },
+        {
+            name: "Pink Princess Dress",
+            imageSrc: "images/pink-dress.jpg",
+            price: 39.99
+        },
+        {
+            name: "Blue Denim Overalls",
+            imageSrc: "images/denim-overalls.jpg",
+            price: 29.99
+        },
+        {
+            name: "Green T-Rex T-shirt",
+            imageSrc: "images/t-rex-tshirt.jpg",
+            price: 19.99
+        },
+        {
+            name: "Red Winter Coat",
+            imageSrc: "images/winter-coat.jpg",
+            price: 49.99
+        },
+        {
+            name: "Pink Princess Dress",
+            imageSrc: "images/pink-dress.jpg",
+            price: 39.99
+        },
+        {
+            name: "Blue Denim Overalls",
+            imageSrc: "images/denim-overalls.jpg",
+            price: 29.99
+        },
+        {
+            name: "Green T-Rex T-shirt",
+            imageSrc: "images/t-rex-tshirt.jpg",
+            price: 19.99
+        },
+        {
+            name: "Red Winter Coat",
+            imageSrc: "images/winter-coat.jpg",
+            price: 49.99
+        },
+        {
+            name: "Pink Princess Dress",
+            imageSrc: "images/pink-dress.jpg",
+            price: 39.99
+        },
+        {
+            name: "Blue Denim Overalls",
+            imageSrc: "images/denim-overalls.jpg",
+            price: 29.99
+        },
+        {
+            name: "Green T-Rex T-shirt",
+            imageSrc: "images/t-rex-tshirt.jpg",
+            price: 19.99
+        },
+        {
+            name: "Red Winter Coat",
+            imageSrc: "images/winter-coat.jpg",
+            price: 49.99
+        },
+       
+        {
+            name: "Yellow Raincoat",
+            imageSrc: "images/raincoat.jpg",
+            price: 24.99
+        },
+        {
+            name: "Elegant Evening Gown",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Floral Summer Dress",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Formal Office Skirt",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "White Wedding Dress",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Bohemian Maxi Dress",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Casual Denim Dress",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Silk Party Gown",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "Vintage Lace Dress",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Chic Cocktail Dress",
+            imageSrc: "images/black-suit.jpg",
+            price: 199.99
+        },
+        {
+            name: "Summer Sundress",
+            imageSrc: "images/blue-shirt.jpg",
+            price: 49.99
+        },
+        {
+            name: "Designer Evening Dress",
+            imageSrc: "images/grey-trousers.jpg",
+            price: 89.99
+        },
+        {
+            name: "Formal Ball Gown",
+            imageSrc: "images/white-tuxedo.jpg",
+            price: 299.99
+        },
+        {
+            name: "Stylish Midi Dress",
+            imageSrc: "images/linen-blazer.jpg",
+            price: 129.99
+        },
+        {
+            name: "Banarasi Silk Saree",
+            imageSrc: "images/banarasi-silk-saree.jpg",
+            price: 79.99
+        },
+        {
+            name: "Kanchipuram Saree",
+            imageSrc: "images/kanchipuram-saree.jpg",
+            price: 89.99
+        },
+        {
+            name: "Chiffon Saree",
+            imageSrc: "images/chiffon-saree.jpg",
+            price: 49.99
+        },
+        {
+            name: "Georgette Saree",
+            imageSrc: "images/georgette-saree.jpg",
+            price: 59.99
+        },
+        {
+            name: "Cotton Saree",
+            imageSrc: "images/cotton-saree.jpg",
+            price: 39.99
+        }
+    ]
+};
+
+// Function to display products based on current search query
+function displayProducts(products) {
+    const productCardsContainer = document.getElementById('product-cards');
+    productCardsContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+            <img src="${product.imageSrc}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>Price: $${product.price.toFixed(2)}</p>
+        `;
+
+        productCardsContainer.appendChild(card);
+    });
+}
+function displayProducts(products) {
+    const productCardsContainer = document.getElementById('product-cards');
+    productCardsContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+            <img src="${product.imageSrc}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>Price: $${product.price.toFixed(2)}</p>
+        `;
+
+        productCardsContainer.appendChild(card);
+    });
+}
+
+function displayProducts(products) {
+    const productCardsContainer = document.getElementById('product-cards');
+    productCardsContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+            <img src="${product.imageSrc}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <p>Price: $${product.price.toFixed(2)}</p>
+        `;
+
+        productCardsContainer.appendChild(card);
+    });
+}
+
+document.getElementById('searchInput').addEventListener('input', function(event) {
+    const searchQuery = event.target.value.toLowerCase();
+    const filteredProducts = Products.filter(product =>
+        product.name.toLowerCase().includes(searchQuery)
+    );
+    displayProducts(filteredProducts);
+});
+
+// Initial display of all products
+displayProducts(Products);
